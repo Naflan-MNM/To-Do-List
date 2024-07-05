6908a8a192902d50c7693f065d168e1f55361abc
Naflan-MNM <naflanm084@gmail.com>
2024-07-05 17:20:56 +0530

    write the logic of progress

--- src/ItemsLoading.js
+++ src/ItemsLoading.js
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 const ItemsLoading = () => {
   return (
-    <div>ItemsLoading</div>
+    <main className='progress'>
+      <CircularProgress />
+    </main>
   )
 }
 
